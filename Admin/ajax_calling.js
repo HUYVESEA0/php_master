e98cfc41b98521e6dff33a150c9904e1b274ad78
HUYVIESEA <hhuy0847@gmail.com>
2024-12-20 00:39:23 +0700

    Refactor AJAX page loading by consolidating loadPage functions into a single function for improved maintainability
    Signed-off-by: HUYVIESEA <hhuy0847@gmail.com>

--- Admin/ajax_calling.js
+++ Admin/ajax_calling.js
@@ -22,17 +22,6 @@ $(document).ready(function() {
         var page = $(this).data('page');
         $('#loader').show();
         setTimeout(function() {
-            if (page.includes('add-')) {
-                loadPageAdd(page);
-            } else if (page.includes('-list')) {
-                loadPageList(page);
-            } else if (page.includes('type-')) {
-                loadPageType(page);
-            } else if (page.includes('payment-')) {
-                loadPagePayment(page);
-            } else {
-                loadPage(page);
-            }
             history.pushState(null, '', '?page=' + page);
         }, 1000);
     });
@@ -55,66 +44,6 @@ $(document).ready(function() {
 function loadPage(page) {
     $.ajax({
         url: '../Admin/Pages/' + page + '.php',
-        type: 'POST',
-        data: {page: page},
-        success: function(data) {
-            console.log(data);
-            $('#content').html(data);
-        },
-        complete: function() {
-            $('#loader').hide();
-        }
-    });
-}
-
-function loadPageAdd(page) {
-    $.ajax({
-        url: '../Admin/Pages/c/' + page + '.php',
-        type: 'POST',
-        data: {page: page},
-        success: function(data) {
-            console.log(data);
-            $('#content').html(data);
-        },
-        complete: function() {
-            $('#loader').hide();
-        }
-    });
-}
-
-function loadPageList(page) {
-    $.ajax({
-        url: '../Admin/Pages/l/' + page + '.php',
-        type: 'POST',
-        data: {page: page},
-        success: function(data) {
-            console.log(data);
-            $('#content').html(data);
-        },
-        complete: function() {
-            $('#loader').hide();
-        }
-    });
-}
-
-function loadPageType(page) {
-    $.ajax({
-        url: '../Admin/Pages/t/' + page + '.php',
-        type: 'POST',
-        data: {page: page},
-        success: function(data) {
-            console.log(data);
-            $('#content').html(data);
-        },
-        complete: function() {
-            $('#loader').hide();
-        }
-    });
-}
-
-function loadPagePayment(page) {
-    $.ajax({
-        url: '../Admin/Pages/api/' + page + '.php', // Corrected path
         type: 'POST',
         data: {page: page},
         success: function(data) {
