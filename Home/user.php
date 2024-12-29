7715ddccb75b024ee0484584bd6dbcab6de68506
HUYVIESEA <hhuy0847@gmail.com>
2024-12-30 04:58:29 +0700

    Implement user session checks and redirect based on user role; update navigation control script for better readability
    Signed-off-by: HUYVIESEA <hhuy0847@gmail.com>

--- Home/user.php
+++ Home/user.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+
+// Check if user is logged in
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ../auth/sign_log.php');
+    exit();
+}
+
+// Check if user role is correct
+if ($_SESSION['role'] === 'admin') {
+    header('Location: ../Admin/index.php');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
--- public/js/nav_control.js
+++ public/js/nav_control.js
@@ -20,19 +20,20 @@ const
         }
     });
 
+
+    
     modeSwitch.addEventListener('click', () => {
         body.classList.toggle('dark');
         modeText.textContent = body.classList.contains('dark') ? 'Light Mode' : 'Dark Mode';
     });
-
 /*time-date*/
-function updateTime() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    time.textContent = `${hours}:${minutes}:${seconds}`;
-}
+    function updateTime() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        time.textContent = `${hours}:${minutes}:${seconds}`;
+    }
 
-setInterval(updateTime, 1000);
-updateTime();
+    setInterval(updateTime, 1000);
+    updateTime();
